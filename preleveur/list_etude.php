6f3d1b5cf89f50c61bf9d86a3ddcc3d1e7123b17
mkherchaoui <m.kherchaoui@yahoo.fr>
2016-06-06 12:17:09 +0200

    Import fichiers medhi

---
+++ preleveur/list_etude.php
@@ -0,0 +1,18 @@
+<html>
+ <head>
+	<meta charset="uft-8">
+  <title>ListePlageEtude</title>
+ </head>	
+ <body>
+	<h1>Liste des Etudes</h1>
+	<div	
+            <table border="1">
+			<td> <tr> Etude1 </tr></td>
+			<td> <tr> Etude2 </tr></td>
+			<td> <tr> Etude3 </tr></td>
+			<td> <tr> Etude4 </tr></td>
+			<td> <tr> Etude5 </tr></td>
+            </table>
+         </div>    
+ </body>
+</html>
---
+++ preleveur/new_zone.php
@@ -0,0 +1,30 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Formulaire POST</title>
+    </head>
+    <body>
+        <form method="post" action="08-POST.php">
+            Nom : <input type="text" name="nom">
+            <br>
+            Latitude A :  <input type="text" name="Latitude A">
+            <br>
+			Longitude A :  <input type="text" name="Longitude A">
+            <br>
+			 Latitude B :  <input type="text" name="Latitude B">
+            <br>
+			Longitude B :  <input type="text" name="Longitude B">
+            <br>
+			 Latitude C :  <input type="text" name="Latitude C">
+            <br>
+			Longitude C :  <input type="text" name="Longitude C">
+            <br>
+			 Latitude D :  <input type="text" name="Latitude D ">
+			 <br>
+			 Longitude D :  <input type="text" name="Longitude D">
+            <br>
+			
+            <input type="submit" value="Valider">
+        </form>
+    </body>
+</html>
