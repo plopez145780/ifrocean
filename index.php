6cb4ee2a8be3cafbd4f051c5e9e06fc94fb1eb0b
plopez <pierre_albert_lopez@hotmail.fr>
2016-06-06 16:09:53 +0200

    Reimporte tous les fichiers

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <?php include("header.php"); ?>
 
-<?php include("menus.php"); ?>
-
 <!-- Le corps -->
+
+<div class="starter-template">
+        <h1>Bootstrap starter template</h1>
+        <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
+      </div>
 <section>
     <h1>Mon super site</h1>
     
