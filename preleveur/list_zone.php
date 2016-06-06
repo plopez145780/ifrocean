bd08ddc956afb92cc21b1f2f7f52b1b79b8ef470
plopez <pierre_albert_lopez@hotmail.fr>
2016-06-06 11:47:21 +0200

    modif liste_zone.php

--- preleveur/list_zone.php
+++ preleveur/list_zone.php
@@ -1,10 +1,15 @@
-<?php include("//header.php"); ?>
+<?php include("../header.php"); ?>
 
-<?php include("//menus.php"); ?>
+<?php include("../menus.php"); ?>
+
+<?php 
+$nomEtude = "nom de l'etude";
+$tabZone = array("zoneA","zoneB","zoneC"); ?>
+
 
 <!-- Le corps -->
 <section>
-    <h1>Liste des zones - <?php $nomEtude ?></h1>
+    <h1>Liste des zones - <?php echo $nomEtude ?></h1>
     
     <table>
         <tr>
@@ -15,10 +20,12 @@
             echo "<tr><td>".$valueZone."</td></tr>";
         }
         ?>
-        
     </table>
+    
+    <!-- link a faire -->
+    <button type="button" class="btn btn-lg btn-default">Nouvelle zone</button>
 </section>
 
 <!-- Le pied de page -->
-<?php include("//footer.php"); ?>
+<?php include("../footer.php"); ?>
     
