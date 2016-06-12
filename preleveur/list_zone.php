a24a2dcf4ecfc08046a650b4e7bdaadb326c2c68
plopez <pierre_albert_lopez@hotmail.fr>
2016-06-12 17:35:07 +0200

    Ajout des liens et des requettes et mise en place de bootstrap

--- preleveur/list_zone.php
+++ preleveur/list_zone.php
@@ -1,10 +1,21 @@
 <?php 
+    $idEtude = filter_input(INPUT_GET, "id_etude", FILTER_SANITIZE_NUMBER_INT);
+    
+    $requete = "SELECT `idZone` AS id, `nomZone` AS nom FROM `zones` WHERE `idEtude`= $idEtude";
+    include_once '../bdd.php';
     $title = "Liste des zones";
     include("../header.inc.php"); 
-    $requete = 'SELECT idEtude, nomEtude, nomVille, superficie, DATE_FORMAT(datePrelevement, \'%d/%m/%Y\') AS datePrelevement_fr, finEtude FROM etudes';
-    include_once '../bdd.php';
+    
+    $nomEtude = "...nom de l'etude...";
+    if ($idEtude == FALSE OR $idEtude == NULL) {
+        $idEtude = 1;
+        echo "<div class='alert alert-danger' role='alert'>
+        <strong>ERREUR</strong> pas de paramèter get
+        </div>";
+    }
+    else {
 
-$nomEtude = "nom de l'etude";
+    
 
 ?>
 
@@ -13,20 +24,19 @@ $nomEtude = "nom de l'etude";
 <section>
     <h1>Liste des zones - <?php echo $nomEtude ?></h1>
     
-    <table>
+    <table class="table table-striped">
         <tr>
             <th>Nom de zone</th>
         </tr>
         <?php while ($donnees = $req->fetch()){
-            echo "<tr><td>".$donnees['nomEtude']."</td></tr>";
+            echo "<tr><td>".$donnees['nom']."</td></tr>";
         }
         ?>
     </table>
     
-    <!-- link a faire -->
-    <button type="button" class="btn btn-lg btn-default">Nouvelle zone</button>
+    <a href="new_zone.php?id_etude=<?php echo $idEtude ?>" class="btn btn-lg btn-default">Nouvelle zone</a>
 </section>
 
 <!-- Le pied de page -->
-<?php include("../footer.inc.php"); ?>
+    <?php } include("../footer.inc.php"); ?>
     
