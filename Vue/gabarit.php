1772e2af33d0a079b65c06fcc018a53f7c6c03ee
Plopez <pierre_albert_lopez@hotmail.fr>
2016-07-24 17:35:00 +0200

    Correction du favicon qui ne fonctionnait pas

--- Vue/gabarit.php
+++ Vue/gabarit.php
@@ -7,7 +7,7 @@
         <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
         <meta name="description" content="">
         <meta name="author" content="">
-        <link rel="icon" href="/favicon.ico">
+        <link rel="shortcut icon" href="favicon.ico" >
         <title><?= $title ?></title>
 
         <link href="/vendor/ifrocean/css/style.css" rel="stylesheet" type="text/css">
